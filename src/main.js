ee48cd8992b5492dffba794d4c15cbc326e35301
nataly-enne <ennenataly@gmail.com>
2019-11-13 17:13:14 -0300

    add: import rtdb plugin

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import "./firebase";
 import Vue from "vue";
 import App from "./App.vue";
 import { firestorePlugin } from "vuefire";
+import { rtdbPlugin } from "vuefire";
+
+Vue.use(rtdbPlugin);
 
 Vue.use(firestorePlugin);
 
